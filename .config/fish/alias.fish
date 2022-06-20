3581b6c4b03fd61b860436aefdea4c1fc6a1f559
Yasser Siddiqui <YS@localhost.localdomain>
2022-06-20 13:59:17 -0400

    add .config/fish/

---
+++ .config/fish/alias.fish
@@ -0,0 +1,5 @@
+# Alias config for use as a git command on dotfiles dir
+alias config='/usr/bin/git --git-dir=$HOME/dotfiles/ --work-tree=$HOME'
+
+# Alias NeoVim shorthand
+alias nv="nvim"
---
+++ .config/fish/config.fish
@@ -0,0 +1,14 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+# Aliases
+if [ -f $HOME/.config/fish/alias.fish ]
+source $HOME/.config/fish/alias.fish
+end
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+eval /home/YS/anaconda3/bin/conda "shell.fish" "hook" $argv | source
+# <<< conda initialize <<<
+
